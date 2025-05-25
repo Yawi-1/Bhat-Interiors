9182c08a3d525c57d0713a0a6dc4c09afdce4094
Yawi <yawimalik786@gmail.com>
2025-05-25 19:43:23 +0530

    Created Ocntact Section

--- js/script.js
+++ js/script.js
@@ -27,30 +27,72 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 // change the services text
 const services = [
-        "PVC Wall Paneling",
-        "Ceiling Wallpaper",
-        "Modular Kitchen",
-        "Aluminium Partition",
-        "Wood Floor",
-        "PVC Matting",
-        "SSS Decor",
-        "SSS Doors",
-        "ACP Sheets",
-        "All Hardware",
-      ];
-      let index = 0;
-      const serviceText = document.getElementById("service-text");
-      setInterval(() => {
-        // Fade out
-        serviceText.style.opacity = 0;
+  "PVC Wall Paneling",
+  "Ceiling Wallpaper",
+  "Modular Kitchen",
+  "Aluminium Partition",
+  "Wood Floor",
+  "PVC Matting",
+  "SSS Decor",
+  "SSS Doors",
+  "ACP Sheets",
+  "All Hardware",
+];
 
-        setTimeout(() => {
-          index = (index + 1) % services.length;
-          serviceText.textContent = services[index];
+let currentService = 0;
+let currentChar = 0;
+const serviceText = document.getElementById("service-text");
 
-          // Fade in
-          serviceText.style.opacity = 1;
-        }, 1000); // Wait for fade-out to complete
-      }, 2500); 
-    //   Gallery Section
+function typeText() {
+  const text = services[currentService];
+  serviceText.textContent = text.substring(0, currentChar + 1);
+  currentChar++;
+
+  if (currentChar < text.length) {
+    setTimeout(typeText, 100); 
+  } else {
+    setTimeout(() => {
+      eraseText();
+    }, 2000); 
+  }
+}
+
+function eraseText() {
+  const text = services[currentService];
+  serviceText.textContent = text.substring(0, currentChar - 1);
+  currentChar--;
+
+  if (currentChar > 0) {
+    setTimeout(eraseText, 50); // erasing speed
+  } else {
+    currentService = (currentService + 1) % services.length;
+    setTimeout(typeText, 500);
+  }
+}
+
+typeText();
+
+
+// Slider Css
+const swiper = new Swiper(".mySwiper", {
+  loop: true,
+  effect: "fade", 
+  fadeEffect: {
+    crossFade: true, 
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next", 
+    prevEl: ".swiper-button-prev",
+  },
+    speed: 1000, 
+});
+
     
